9b0238feaba80cce5ebde9627790e4a2165b3de1
ignurof <ignurof@ignurof.com>
2023-12-30 15:06:31 +0100

    archive work

---
+++ frontend/js/archive.js
@@ -0,0 +1,34 @@
+let domArchive = document.getElementsByClassName("archive");
+
+const currentYear = 2023;
+
+const showCurrentYear = () => {
+    let domArticleList = document.getElementById("article-list");
+
+    let articleListTest = [1, 2, 3];
+
+    for (let i = 0; i < articleListTest.length; i++) {
+        const element = articleListTest[i];
+
+        domArticleList.insertAdjacentHTML("beforeend", `
+            <li>
+                <a href="article.html?id=0" target="_blank" rel="noreferrer">Article Title Here</a>
+            </li>
+        `);
+    }
+}
+
+showCurrentYear();
+
+/*
+<section class="archive">
+    <h1><u>Archive</u></h1>
+    <label for="year">Year</label>
+    <select name="year">
+        <option value="2023" onclick="showToggle(2023)">2023</option>
+    </select>
+    <!-- <button onclick="showToggle(2023)">2023</button> -->
+    <ul id="article-list"></ul>
+</section>
+*/
+
