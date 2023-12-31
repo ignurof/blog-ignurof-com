96632b0418efe1842116eac329e98d6867132640
ignurof <ignurof@ignurof.com>
2023-12-31 14:17:43 +0100

    savecheckpoint

--- backend/main.js
+++ backend/main.js
@@ -4,23 +4,28 @@ const cors = require("cors");
 const app = express();
 const port = 3420;
 
+const articles = require("./articles.js");
+
 const corsOptions = {
     origin: 'http://localhost',
     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-    methods: "GET",
+    methods: "OPTIONS,GET",
     allowedHeaders: "Content-Type",
 };
 
 /*
 You can also enable pre-flight across-the-board like so:
-
 app.options('*', cors()) // include before other routes
-
 NOTE: When using this middleware as an application level middleware (for example, app.use(cors())), pre-flight requests are already handled for all routes.
 */
 
 // server wide cors
 app.use(cors());
+
+// Start server and listen to incoming connections
+app.listen(port, () => {
+    console.log(`Server listening on port: ${port}`);
+});
 
 app.get("/", (request, response) => {
     console.log(request);
@@ -30,7 +35,5 @@ app.get("/", (request, response) => {
     response.json(helloWorld);
 });
 
-// Start server and listen to incoming connections
-app.listen(port, () => {
-    console.log(`Server listening on port: ${port}`);
-});
+// Endpoint routing
+app.use("/articles", articles);
