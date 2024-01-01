4389a955c0c720aa04e727924ec412fb7176db4c
ignurof <ignurof@ignurof.com>
2024-01-01 13:20:14 +0100

    read articles in dir

--- backend/articles.js
+++ backend/articles.js
@@ -1,4 +1,60 @@
+const express = require("express");
+const fs = require("node:fs/promises");
+const router = express.Router();
+
+// middleware that is specific to this router
+router.use((request, response, next) => {
+    console.log("Time: ", Date.now());
+    next();
+});
+
+router.get('/', (request, response) => {
+    console.log("articles/");
+
+    readArticlesFromDir();    
+
+    response.json({hello: "world"});
+});
+
+router.get("/list", (request, response) => {
+    console.log("articles/list/");
+
+    response.json(getArticles());
+});
+
+router.get("/create", (request, response) => {
+    console.log("articles/create/");
+    
+    // TODO
+
+    response.json({created: true});
+});
+
 let articles = [{}];
+
+const getArticles = () => {
+    return articles;
+}
+
+const readArticlesFromDir = async () => {
+    try{
+        const dir = await fs.opendir("./articles/");
+        for await (file of dir){
+            console.log(file);
+        }
+    }
+    catch(error){
+        console.error(error);
+    }
+
+    /*
+    fs.readFile("./articles/test_article.json", (error, data) => {
+        if(error) throw error;
+        
+        console.log(JSON.parse(data));
+    });
+    */
+}
 
 const articleTemplate = {
     title: "This is a title",
@@ -22,3 +78,4 @@ articleOne.paragraphs[0].text = "This is the paragraph content text. Scaled down
 articleOne.paragraphs[0].img = "https://picsum.photos/200";
 article = articleOne;
 
+module.exports = router;
