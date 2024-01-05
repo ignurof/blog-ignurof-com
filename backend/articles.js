4721183ec143169e8b35b7fefe73afdb1e9e06e8
ignurof <ignurof@ignurof.com>
2024-01-05 01:57:20 +0100

    return specific article by id

--- backend/articles.js
+++ backend/articles.js
@@ -12,27 +12,28 @@ const setup = async () => {
 
 // middleware that is specific to this router
 router.use((request, response, next) => {
-    console.log("Time: ", Date.now());
+    console.log("Route Request @ Time since Epoch: ", Date.now());
     next();
 });
 
 router.get('/', (request, response) => {
-    console.log("articles/");
-
     response.json({hello: "world"});
 });
 
 router.get("/list", (request, response) => {
-    console.log("articles/list/");
-    console.log(getArticles());
     response.json(getArticles());
 });
 
-router.get("/create", (request, response) => {
-    console.log("articles/create/");
-    
-    // TODO
+// order is important, if this is placed above /list, then this would get called and print id: "list"
+router.get("/:id", (request, response) => {
+    // TODO: Sanitize more
+    if(!isNumericIntFromString(request.params.id)) return console.error("not numeric int");
+     
+    response.json(getArticleById(request.params.id));
+});
 
+router.get("/create", (request, response) => {
+    // TODO
     response.json({created: true});
 });
 
@@ -45,39 +46,39 @@ const getArticles = () => {
 const setArticles = (value) => {
     articles = value;
 }
+
+// articles should be index-sorted by id, so id should always be the index
+const getArticleById = (id) => {
+    return getArticles()[id];
+}
  
 const readArticlesFromDir = async () => {
     let fileNames = []; 
     let fileArticles = [];
 
-    try{
-        const dir = await fs.opendir("./articles/");
-        for await (file of dir){
-            fileNames.push(file.name);
-        }
-    }
-    catch(error){
-        console.error(error);
+    const dir = await fs.opendir("./articles/");
+    for await (file of dir){
+        fileNames.push(file.name);
     }
 
     for(let i = 0; i < fileNames.length; i++){
-        try{
-            let filePath = `./articles/${fileNames[i]}`;
-            let fileContentsBuffer = await fs.readFile(filePath);
-            let fileContents = JSON.parse(fileContentsBuffer);
-            fileArticles.push({
-                title: fileContents.title,
-                paragraphs: fileContents.paragraphs,
-                date: fileContents.date,
-                id: fileContents.id,
-            });
-        } 
-        catch(error){
-            console.error(error);
-        }
+        let filePath = `./articles/${fileNames[i]}`;
+        let fileContentsBuffer = await fs.readFile(filePath);
+        let fileContents = JSON.parse(fileContentsBuffer);
+        fileArticles.push({
+            title: fileContents.title,
+            paragraphs: fileContents.paragraphs,
+            date: fileContents.date,
+            id: fileContents.id,
+        });
     }
 
     return fileArticles;
+}
+
+// returns true if the string contains an integer
+const isNumericIntFromString = (str) => {
+    return /-?\d+$/.test(str);
 }
 
 // I would prefer to pass this is as an object but when using router in express like this, it doesnt like it
